6c89547baef3aa3134e8446aa0130983355fae39
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-02-07 18:31:37 +0530

    Time: 43 ms (34.21%), Space: 47.8 MB (45.39%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,16 +1,50 @@
 class Solution {
     public int minimumDeletions(String s) {
-        Stack<Character> st= new Stack<>();
-        int cnt=0;
         int n=s.length();
+        int a_c=0;
+        int b_c=0;
+        int del=Integer.MAX_VALUE;
+
+        if(n==1 && (s.charAt(0)=='a'||s.charAt(0)=='b')){
+            return 0;
+        }
+
         for(int i=0; i<n; i++){
-            if(!st.isEmpty()&&(s.charAt(i)=='a' && st.peek()=='b')){
-                st.pop();
-                cnt++;
-            }else{
-                st.push(s.charAt(i));
+            if(s.charAt(i)=='a'){
+                a_c++;
             }
         }
-        return cnt;
+
+        for(int i=0; i<n; i++){
+            del=Math.min(del,a_c+b_c);
+            if(s.charAt(i)=='a'){
+                a_c--;
+            }
+            else if(s.charAt(i)=='b'){
+                b_c++;
+            }
+        }
+        del = Math.min(del, a_c + b_c);
+
+        return del;
     }
 }
+
+
+
+
+
+
+
+        // Stack<Character> st= new Stack<>();
+        // int cnt=0;
+        // int n=s.length();
+        // for(int i=0; i<n; i++){
+        //     if(!st.isEmpty()&&(s.charAt(i)=='a' && st.peek()=='b')){
+        //         st.pop();
+        //         cnt++;
+        //     }else{
+        //         st.push(s.charAt(i));
+        //     }
+        // }
+        // return cnt;
