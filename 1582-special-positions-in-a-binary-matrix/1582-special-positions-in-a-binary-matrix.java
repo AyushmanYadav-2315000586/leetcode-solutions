1b8e82536031a9eb9761afc736f57b5cdc86a1b1
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-03-04 19:33:48 +0530

    Time: 2 ms (94.84%), Space: 47.2 MB (65.84%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,33 +1,73 @@
 class Solution {
-    int m,n;
     public int numSpecial(int[][] mat) {
         int cnt=0;
-        m=mat.length;
-        n=mat[0].length;
+        int m=mat.length;
+        int n=mat[0].length;
+
+        int []row= new int [m];
+        int []col= new int [n];
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j]==1){
-                    if(check(mat,i,j)){
-                        cnt++;
-                    }
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==1 && row[i]==1 && col[j]==1){
+                    cnt++;
                 }
             }
         }
         return cnt;
     }
-
-    boolean check(int[][]mat, int row, int col){
-        for(int j=0; j<n; j++){
-            if(j!=col && mat[row][j]==1){
-                return false;
-            }
-        }
-        for(int i=0; i<m; i++){
-            if(i!=row && mat[i][col]==1){
-                return false;
-            }
-        }
-        return true;
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     int m,n;
+//     public int numSpecial(int[][] mat) {
+//         int cnt=0;
+//         m=mat.length;
+//         n=mat[0].length;
+
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 if(mat[i][j]==1){
+//                     if(check(mat,i,j)){
+//                         cnt++;
+//                     }
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+
+//     boolean check(int[][]mat, int row, int col){
+//         for(int j=0; j<n; j++){
+//             if(j!=col && mat[row][j]==1){
+//                 return false;
+//             }
+//         }
+//         for(int i=0; i<m; i++){
+//             if(i!=row && mat[i][col]==1){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
