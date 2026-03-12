2bafe3c3831f298a625c79812c8d3e158249f9b9
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-03-12 16:14:26 +0530

    Time: 0 ms (100%), Space: 80.4 MB (56.13%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -7,7 +7,7 @@ class Solution {
             if(mid>0 && mid<r && arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
                 return mid;
             }
-            else if(mid>0 && arr[mid]<arr[mid-1]){
+            else if(mid>0 && mid<r&&arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1]){
                 r=mid-1;
             }
             else{
