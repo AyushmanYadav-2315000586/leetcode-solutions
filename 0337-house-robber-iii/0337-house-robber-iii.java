c7789e5925f23cb9f15d8528c55a7396a06f658f
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-03-13 15:47:01 +0530

    Time: 2 ms (33.11%), Space: 46.6 MB (72.11%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<TreeNode, Integer> map= new HashMap<>();
+    public int rob(TreeNode root) {
+        if(root==null) return 0;
+        if(map.containsKey(root)){
+            return map.get(root);
+        }
+        int take=root.val;
+        if(root.left!=null){
+            take+=rob(root.left.left)+rob(root.left.right);
+        }
+        if(root.right!=null){
+            take+=rob(root.right.left)+rob(root.right.right);
+        }
+
+        int skip=rob(root.left)+rob(root.right);
+        int max= Math.max(take,skip);
+        map.put(root,max);
+        return max;
+    }
+}
