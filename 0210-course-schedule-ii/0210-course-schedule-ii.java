b2186a1819a0572889421947f053589a75a1c541
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-03-19 23:14:41 +0530

    Time: 7 ms (34.76%), Space: 47.1 MB (34.73%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int n, int[][] preq) {
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int []pre: preq){
+            int u=pre[0];
+            int v=pre[1];
+
+            adj.get(v).add(u);
+        }
+        int []idg= new int[n];
+        for(int i=0; i<n; i++){
+            for(int nei: adj.get(i)){
+                idg[nei]++;
+            }
+        }
+        Queue<Integer> q= new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(idg[i]==0){
+                q.add(i);
+            }
+        }
+        int cnt=0;
+        int []topo= new int[n];
+        int i=0;
+        while(!q.isEmpty()){
+            int node=q.remove();
+            topo[i++]=node;
+            cnt++;
+            for(int it: adj.get(node)){
+                idg[it]--;
+                if(idg[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+
+        return cnt==n?topo:new int[]{};
+    }
+}
