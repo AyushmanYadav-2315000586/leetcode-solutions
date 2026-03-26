ab6f54307b02620ecd5f544fd6fcbd465b7cd788
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-03-27 03:34:27 +0530

    Time: 240 ms (78.38%), Space: 240.7 MB (89.19%) - LeetHub

---
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.java
@@ -0,0 +1,78 @@
+class Solution {
+    long total=0;
+    public boolean canPartitionGrid(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                total+=grid[i][j];
+            }
+        }
+
+        if(checkHorCuts(grid)){
+            return true;
+        }
+
+        reverse(grid);
+
+
+        if(checkHorCuts(grid)) return true;
+
+        reverse(grid);
+
+        int[][]tG=new int[n][m];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                tG[j][i]=grid[i][j];
+            }
+        }
+
+        if(checkHorCuts(tG)) return true;
+
+        reverse(tG);
+
+        return checkHorCuts(tG);
+
+    }
+    void reverse(int[][]grid){
+        int top=0, bottom=grid.length-1;
+
+        while(top<bottom){
+            int []temp=grid[top];
+            grid[top]=grid[bottom];
+            grid[bottom]=temp;
+
+            top++;
+            bottom--;
+        }
+    }
+
+    boolean checkHorCuts(int[][]grid){
+        int m=grid.length;
+        int n=grid[0].length;
+
+        HashSet<Long> set= new HashSet<>();
+
+        long top=0;
+
+        for(int i=0; i<m-1; i++){
+            for(int j=0; j<n; j++){
+                set.add((long) grid[i][j]);
+                top+=grid[i][j];
+            }
+
+            long bottom=total-top;
+            long diff=top-bottom;
+
+            if(diff==0) return true;
+            if(diff==grid[0][0]) return true;
+            if(diff==grid[0][n-1]) return true;
+            if(diff==grid[i][0]) return true;
+            if(i>0 && n>1 && set.contains(diff)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
