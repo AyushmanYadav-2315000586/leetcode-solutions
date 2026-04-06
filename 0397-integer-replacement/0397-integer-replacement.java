dfa1579c2da10ab417551321e3d954080cbc55b1
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-04-06 20:36:34 +0530

    Time: 7 ms (7.94%), Space: 42.1 MB (50.1%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,21 +1,46 @@
 class Solution {
-    public int integerReplacement(int nu) {
-        int cnt=0;
-        long n=nu;
-        while(n>1){
-            if(n%2==0){
-                n/=2;
-            }
-            else{
-                if((n==3)||(n&2)==0){
-                    n--;
-                }
-                else{
-                    n++;
-                }
-            }
-            cnt++;
+    int ans=0;
+    public int integerReplacement(int n) {
+        return solve((long)n);
+    }
+
+    int solve(long n){
+        if(n==1){
+            return 0;
         }
-        return cnt;
+
+        if(n%2==0){
+            ans=1+solve(n/2);
+        }
+        else{
+            int minus=solve(n-1);
+            int plus=solve(n+1);
+            ans=1+Math.min(minus,plus);
+        }
+
+        return ans;
     }
 }
+
+
+// class Solution {
+//     public int integerReplacement(int nu) {
+//         int cnt=0;
+//         long n=nu;
+//         while(n>1){
+//             if(n%2==0){
+//                 n/=2;
+//             }
+//             else{
+//                 if((n==3)||(n&2)==0){
+//                     n--;
+//                 }
+//                 else{
+//                     n++;
+//                 }
+//             }
+//             cnt++;
+//         }
+//         return cnt;
+//     }
+// }
