2b3694479a45c5262c41cbade9da1027af8c2136
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-05-03 05:04:07 +0530

    Time: 5 ms (34.62%), Space: 42.1 MB (70.14%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.java
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -2,24 +2,65 @@ class Solution {
     public int rotatedDigits(int n) {
         int cnt=0;
         for(int i=1; i<=n; i++){
-            if(isGood(i)){
+            if(isGood(i)==1){
                 cnt++;
             }
         }
-        return cnt;
+        return cnt;    
     }
-    boolean isGood(int num){
-        boolean ch=false;
-        while(num>0){
-            int d=num%10;
-            if(d==3||d==4||d==7){
-                return false;
-            }
-            if(d==2||d==5||d==6||d==9){
-                ch=true;;
-            }
-            num/=10;
+    int isGood(int num){
+        if(num==0){
+            return 0;
         }
-        return ch;
+        int r=isGood(num/10);
+        int d=num%10;
+        int d_c;
+        if(d==0||d==1||d==8){
+            d_c=0;
+        }
+        else if(d==2||d==5||d==6 ||d==9){
+            d_c=1;
+        }
+        else{
+            return 2;
+        }
+        if(r==2){
+            return 2;
+        }
+        if(r==0 && d_c==0){
+            return 0;
+        }
+
+        return 1;
     }
+
 }
+
+
+
+
+// class Solution {
+//     public int rotatedDigits(int n) {
+//         int cnt=0;
+//         for(int i=1; i<=n; i++){
+//             if(isGood(i)){
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+//     boolean isGood(int num){
+//         boolean ch=false;
+//         while(num>0){
+//             int d=num%10;
+//             if(d==3||d==4||d==7){
+//                 return false;
+//             }
+//             if(d==2||d==5||d==6||d==9){
+//                 ch=true;;
+//             }
+//             num/=10;
+//         }
+//         return ch;
+//     }
+// }
