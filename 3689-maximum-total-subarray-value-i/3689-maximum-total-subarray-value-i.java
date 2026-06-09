79cfbe1f496af6c057cffeadc3263d1754bf6cc6
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-06-09 12:29:01 +0530

    Time: 1 ms (100%), Space: 62.5 MB (20.14%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -4,6 +4,7 @@ class Solution {
         int left = 0;
         int max = nums[0];
         int min = nums[0];
+        
         for(int i = 1;i<n;i++){
             max = Math.max(nums[i],max);
             min = Math.min(nums[i],min);
