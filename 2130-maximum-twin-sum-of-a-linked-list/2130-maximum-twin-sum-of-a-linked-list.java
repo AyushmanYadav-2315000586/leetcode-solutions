c03c02996ba94f96122d766bcea3a1d88cd83e0d
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-06-15 22:56:56 +0530

    Time: 9 ms (26.19%), Space: 102.6 MB (30.2%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,16 +10,93 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        ArrayList<Integer> num= new ArrayList<>();
-        while(head!=null){
-            num.add(head.val);
-            head=head.next;
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
         }
+        ListNode curr=rev(slow);
         int res=0;
-        int n=num.size();
-        for(int i=0; i<n/2; i++){
-            res=Math.max(res,num.get(i)+num.get(n-1-i));
+        while(curr!=null){
+            res=Math.max(res,head.val+curr.val);
+            curr=curr.next;
+            head=head.next;
         }
         return res;
     }
+
+    ListNode rev(ListNode head){
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        ListNode temp= rev(head.next);
+        ListNode front=head.next;
+        front.next=head;
+        head.next=null;
+        return temp;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         Stack<Integer> st= new Stack<>();
+//         ListNode dum=head;
+//         while(dum!=null){
+//             st.push(dum.val);
+//             dum=dum.next;
+//         }
+//         int res=0;
+//         int n=st.size();
+//         int i=0;
+//         while(i<n/2){
+//             int v=st.pop();
+//             res=Math.max(res,head.val+v);
+//             i++;
+//             head=head.next;
+//         }
+//         return res;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         ArrayList<Integer> num= new ArrayList<>();
+//         while(head!=null){
+//             num.add(head.val);
+//             head=head.next;
+//         }
+//         int res=0;
+//         int n=num.size();
+//         for(int i=0; i<n/2; i++){
+//             res=Math.max(res,num.get(i)+num.get(n-1-i));
+//         }
+//         return res;
+//     }
+// }
