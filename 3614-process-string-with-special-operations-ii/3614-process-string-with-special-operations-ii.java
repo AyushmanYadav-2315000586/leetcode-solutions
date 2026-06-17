18ca4217c1524966ed14404addc2da8e387d1c25
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-06-18 01:23:57 +0530

    Time: 30 ms (69.92%), Space: 48.1 MB (14.29%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public char processStr(String s, long k) {
+        int n=s.length();
+
+        long l=0;
+        for(int i=0; i<n; i++){
+            char ch=s.charAt(i);
+
+            if(ch=='*'){
+                if(l>0){
+                    l--;
+                }
+            }
+            else if(ch=='#'){
+                l*=2;
+            }
+            else if(ch=='%'){
+                continue;
+            }
+            else{
+                l++;
+            }
+        }
+
+        if(k>=l){
+            return '.';
+        }
+
+        for(int i=n-1; i>=0; i--){
+            char ch=s.charAt(i);
+
+            if(ch=='*'){
+                l++;
+            }
+            else if(ch=='#'){
+                l/=2;
+                k=(k>=l)?k-l:k;
+            }
+            else if(ch=='%'){
+                k=l-k-1;
+            }
+            else{
+                l--;
+            }
+
+            if(k==l){
+                return ch;
+            }
+        }
+        return '.';
+    }
+}
