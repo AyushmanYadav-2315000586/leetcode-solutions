e634cc262a68bf1d7b7e0a1ffb900f8701e34871
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-07-27 10:51:27 +0530

    Time: 2 ms (76.63%), Space: 47.5 MB (17%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,12 +3,7 @@ class Solution {
         int m=matrix.length;
         int n=matrix[0].length;
         int min=Integer.MAX_VALUE;
-        if(m==1){
-            for(int i=0; i<n; i++){
-                min=Math.min(min,matrix[0][i]);
-            }
-            return min;
-        };
+        
         int[][]dp= new int[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
