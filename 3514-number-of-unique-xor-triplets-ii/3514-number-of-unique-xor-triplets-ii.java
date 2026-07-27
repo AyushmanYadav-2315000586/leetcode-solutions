28d12502922f598e02ddb4c60f33b6649d127ff0
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-07-27 16:50:59 +0530

    Time: 253 ms (52.27%), Space: 46.9 MB (77.38%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,18 +1,38 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        HashSet<Integer> set= new HashSet<>();
         int n=nums.length;
+        int max=max(nums);
+        int t=1;
+        while(t<=max){
+            t*=2;
+        }
+        boolean []arr= new boolean[t];
         for(int i=0; i<n; i++){
             for(int j=i; j<n; j++){
-                set.add(nums[i]^nums[j]);
+                arr[nums[i]^nums[j]]=true;
             }
         }
-        HashSet<Integer>ans= new HashSet<>();
-        for(int val:set){
-            for(int num:nums){
-                ans.add(val^num);
+        boolean []ans= new boolean[t];
+        for(int i=0; i<t; i++){
+            if(arr[i]){
+                for(int val: nums){
+                    ans[i^val]=true;
+                }
             }
         }
-        return ans.size();
+        int a=0;
+        for(int i=0; i<t; i++){
+            if(ans[i]){
+                a++;
+            }
+        }
+        return a;
+    }
+    int max(int nums[]){
+        int val=Integer.MIN_VALUE;
+        for(int num:nums){
+            val=Math.max(num,val);
+        }
+        return val;
     }
 }
