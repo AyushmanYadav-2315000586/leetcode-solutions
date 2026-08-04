79e56e450e6ba51bedfe416ed82d2e8f42494949
Ayushman Yadav <138818041+AyushmanYadav-2315000586@users.noreply.github.com>
2026-08-04 07:23:30 +0530

    Time: 2 ms (11.73%), Space: 43.7 MB (5.08%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,11 +1,17 @@
 class Solution {
     public int minimumPushes(String word) {
-        int p=0;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        int as=2;
+        int res=0;
+        for(char ch: word.toCharArray()){
+            if(as>9){
+                as=2;
+            }
 
-        for(int i=0; i<word.length(); i++){
-            p+=(i/8)+1;
+            map.put(as,map.getOrDefault(as,0)+1);
+            res+=map.get(as);
+            as++;
         }
-
-        return p;
+        return res;
     }
 }
